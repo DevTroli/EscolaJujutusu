c6506c12ade71c175472601d2dc1da5956da229e
Troli <brawlixo123@gmail.com>
2026-09-16 11:29:13 -0300

    First commit: adicionando estrutura da API SpringBoot

---
+++ src/main/java/com/jujutsuKaisen/escolajujutsu/EscolajujutsuApplication.java
@@ -0,0 +1,12 @@
+package com.jujutsuKaisen.escolajujutsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolajujutsuApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EscolajujutsuApplication.class, args);
+  }
+}
---
+++ src/test/java/com/jujutsuKaisen/escolajujutsu/EscolajujutsuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jujutsuKaisen.escolajujutsu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolajujutsuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
